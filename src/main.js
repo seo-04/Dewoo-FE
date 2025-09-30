5667ee9cc563d02a952e532ac7816021550e6595
tomhoon <gnsdl9108@naver.com>
2025-09-30 15:50:22 +0900

    fix: 전역으로 사용되는 스타일 임시 주석처리

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-import './assets/css/Login.css'
-import './assets/css/PasswordSetting.css'
-import './assets/css/FindPassword.css'
-import './assets/css/PaymentMethod.css'
-import './assets/css/SignUp.css'
-import './assets/css/Authenticate.css'
-import './assets/css/HotelAccommodation.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
