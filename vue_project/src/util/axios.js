bd18f20b229217929b8e8caed1ac748de82f86d1
seo-04 <coups1234@naver.com>
2025-10-01 20:06:09 +0900

    hotellisting 수정

---
+++ vue_project/src/util/axios.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+console.log('baseUrl : ' + process.env.VUE_APP_API_URL);
+
+export const bTeamApi = axios.create({
+    baseURL: process.env.VUE_APP_API_URL
+});
+
+await bTeamApi.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token")
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    error => Promise.reject(error)
+)
+
+export default bTeamApi
