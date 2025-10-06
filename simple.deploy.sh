5f57537a0af8f83e92670ab8ee43eee013a9d727
tomhoon <gnsdl9108@naver.com>
2025-10-06 11:50:07 +0900

    test: simple deploy sh added

---
+++ simple.deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e;
+
+echo "⛳️ 도커 컨테이너 중지"
+docker stop front-container
+
+echo "⛳️ 도커 컨테이너 삭제"
+docker rm front-container
+
+echo "⛳️ 도커 이미지 삭제"
+docker rmi front-image
+
+echo "⛳️ 도커 이미지 생성"
+docker build -t front-image .
+
+echo "⛳️ 도커 컨테이너 생성"
+docker run -d -p 780:80 --name front-container --net daewoo_a_network front-image
