27be67169e25872368b56779a78c10f1dad3d71d
Lee-HaeSung <haesung116@naver.com>
2025-11-03 17:34:12 +0900

    feat. favorite페이지 버튼기능 추가

--- src/components/favorite/FavoriteItem.vue
+++ src/components/favorite/FavoriteItem.vue
@@ -213,10 +213,10 @@ img{ width: 10px; height: 14px}
         height: 48px;
         padding: 15px 300px;
         font-size: 14px;
-        font-weight: 500;
+        font-weight: 600;
         background-color: rgba(141, 211, 187, 1);
         border-radius: 5px;
-        font-weight: 600;
+        cursor: pointer;
       }
     }
 
